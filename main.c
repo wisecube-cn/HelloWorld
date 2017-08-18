700f7faa93a8444f50530de82e0e2ed8416b50a6
hnjzsdx <2428350606@qq.com>
2017-08-18 16:23:55 +0800

    ADD systemInit() & delayMs()

--- main.c
+++ main.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
 
+/*Main
+*	ADD systemInit();
+*	ADD delayMs();
+*	
+*	version:1.1
+*	date:	2017/08/08
+*	Author:	hnjzsdx
+*/
 int main(){
+	systemInit();//Init the board
+	printf("systemInit OK!\n");
+	delayMs(1000);
+	
 	printf("HelloWorld!\n");
 	
 	return 0;
+}
+
+/*Init all the system.
+*	change the time of delay from 1000 to 500
+*
+*	version:1.1
+*	date:	2017/08/08
+*	Author:	hnjzsdx
+*/
+void systemInit(){
+	printf("systemInit ing\n");
+	delayMs(500);
+	
+}
+
+/*delayMs delay a while
+*	
+*	version:1.0
+*	date:	2017/08/08
+*	Author:	hnjzsdx
+*/
+void delayMs(int n){
+	while(n--){
+		;
+	}
 }
